762ac7a57299574c41436cce6c00a29673e0bb94
김태환3 [kimsalt] <maxtortime@gmail.com>
2023-06-12 11:04:18 +0900

    PromptForm 추가

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,8 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class PromptForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title', 'prompt', 'image_prompt')
